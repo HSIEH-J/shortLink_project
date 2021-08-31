fa4d55c61030dd716db2118086781b8119537ddd
YICHEN HSIEH <boisw3641@gmail.com>
2021-08-31 23:07:35 +0800

    fix: delete auth middleware for testing

--- server/routes/short_link_route.js
+++ server/routes/short_link_route.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const { generateShortLink } = require("../controller/short_link_controller");
-const { authentication } = require("../../util/util");
-
-router.route("/link").post(authentication(), generateShortLink);
+// const { authentication } = require("../../util/util");
+// authentication(),
+router.route("/link").post(generateShortLink);
 // router.route("/:url").get(findUrl);
 
 module.exports = router;
