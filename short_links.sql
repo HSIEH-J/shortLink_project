77540bbbd5bdac3ba91bcd9fb35297a4212d9e58
YICHEN HSIEH <boisw3641@gmail.com>
2021-09-02 17:22:45 +0800

    docs: env template && export db

--- short_links.sql
+++ short_links.sql
@@ -16,6 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `links`
+--
+
+DROP TABLE IF EXISTS `links`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `links` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `original_url` varchar(255) DEFAULT NULL,
+  `short_link` varchar(255) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `times` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `short_link` (`short_link`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `links`
+--
+
+LOCK TABLES `links` WRITE;
+/*!40000 ALTER TABLE `links` DISABLE KEYS */;
+INSERT INTO `links` VALUES (9,'https://www.fooish.com/javascript/cookie.html','1cldkg',NULL,2),(10,'https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/String/Trim','iwtrpg',NULL,0),(11,'https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/String/Trim','5lcjqd',NULL,0),(12,'https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/String/Trim','nzp9t0',NULL,1),(13,'https://socket.io/get-started/chat','8lmwx9',NULL,0),(14,'https://socket.io/get-started/chat','q4jvfw',NULL,0),(15,'https://github.com/HSIEH-J','c63n45',NULL,0),(16,'https://github.com/HSIEH-J','modjdr',NULL,0),(17,'https://github.com/HSIEH-J','rybyb9',NULL,0),(18,'https://github.com/HSIEH-J','cka7yj',NULL,0),(19,'https://github.com/HSIEH-J','booa8j',NULL,0),(20,'https://github.com/HSIEH-J','ity0iw',NULL,0),(21,'https://github.com/HSIEH-J','2pq2hb',NULL,1),(22,'https://github.com/HSIEH-J','kpssi5',NULL,0),(23,'https://github.com/HSIEH-J','cisyww',NULL,0),(24,'https://github.com/HSIEH-J','gh7667',NULL,0),(25,'https://github.com/HSIEH-J','qvezv9',NULL,1),(26,'https://github.com/HSIEH-J','21kiqb',NULL,0),(27,'https://github.com/HSIEH-J','cneg7l',NULL,1),(28,'https://github.com/HSIEH-J','vhcdig',NULL,2),(29,'https://github.com/HSIEH-J','gu97x6',NULL,1),(30,'https://github.com/HSIEH-J','pv305s',NULL,0),(31,'https://github.com/HSIEH-J','lf4noq',NULL,1),(32,'https://github.com/HSIEH-J','vrflgy',NULL,0),(33,'https://github.com/HSIEH-J','mv83i9',NULL,0),(34,'https://github.com/HSIEH-J','nqntgg',NULL,0),(35,'https://github.com/HSIEH-J','a8qsjb',NULL,0),(36,'https://github.com/HSIEH-J','yyrf42',NULL,0),(37,'https://github.com/HSIEH-J','pr0iab',NULL,2),(38,'https://github.com/HSIEH-J','bqrwww',NULL,0),(39,'https://github.com/HSIEH-J','vp93w0',NULL,1),(40,'https://www.fooish.com/javascript/cookie.html','3dkj1c',NULL,0);
+/*!40000 ALTER TABLE `links` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `sessions`
 --
 
@@ -36,7 +64,6 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('CJtvnPl00OjPBRjRGx7IS3J2Hm8Y-jqN',1630135940,'{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"passport\":{\"user\":4}}');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +79,7 @@ CREATE TABLE `user` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,7 +88,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (4,'aaa@gmail.com','$2b$10$8UozNonT5ImlxyUTihhlH.4aHZe0FmV.lfv0SYdmQ9mzYor/0yOA2'),(5,'bbb@gmail.com','$2b$10$x60ULAV/e1Mp3.LQiMB8Y.ePSiFy3vAb7il13atVQvIXYc7ZvzhGO'),(6,'ccc@gmail.com','$2b$10$W4GYkeMME6jc.cYfZLDDw.7JN3qGoiR3lkNi9I7rcUhweTpjEZQzm');
+INSERT INTO `user` VALUES (4,'aaa@gmail.com','$2b$10$8UozNonT5ImlxyUTihhlH.4aHZe0FmV.lfv0SYdmQ9mzYor/0yOA2'),(5,'bbb@gmail.com','$2b$10$x60ULAV/e1Mp3.LQiMB8Y.ePSiFy3vAb7il13atVQvIXYc7ZvzhGO'),(6,'ccc@gmail.com','$2b$10$W4GYkeMME6jc.cYfZLDDw.7JN3qGoiR3lkNi9I7rcUhweTpjEZQzm'),(7,'ddd@gmail.com','$2b$10$CRP5immrO6tvIou/N1/Oyu94WvpRk699A3KgMRemx0ZSyBLHWXR92'),(8,'fff@gmail.com','$2b$10$9DIvYzpQoyH1wHamcCVwbOxPiIm03zsm75wcNTjNgZ3I6PVoAsxpe');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -74,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-30 23:16:27
+-- Dump completed on 2021-09-02 17:21:18
